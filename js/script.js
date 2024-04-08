4c13e18e2a0625ff36fecb8f3ac7a56e29ae7bca
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-08 18:33:48 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -13,7 +13,23 @@ for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", function() {
         firstNum += numbers[i].innerHTML;
         console.log(numbers[i].innerHTML);
-        zero.innerHTML += numbers[i].innerHTML;
-        // zero.classList.add("hidden");
-    });
+        zero.classList.add("hidden");
+        display.innerHTML += numbers[i].innerHTML;
+    })
 }
+
+// aggiungere event listener a tutti gli operatori
+// quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
+
+const operators = document.querySelectorAll(".operations");
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function() {
+        curOperator = operators[i].innerHTML;
+        console.log(operators[i]);
+
+        display.innerHTML = curOperator;
+        ;
+    })
+}
+
